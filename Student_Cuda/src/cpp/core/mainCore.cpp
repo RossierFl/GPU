875c7ef45bc92d7006347aee34c4bc3123ffeabe
Loic Monney <loic.monney@hefr.ch>
2015-01-05 12:49:09 +0100

    Update activate histogramme

--- Student_Cuda/src/cpp/core/mainCore.cpp
+++ Student_Cuda/src/cpp/core/mainCore.cpp
@@ -7,13 +7,15 @@ using std::endl;
 //extern bool useHello();
 //extern void useAdd();
 //extern bool usePI();
-extern bool useMontecarlo();
+//extern bool useMontecarlo();
+extern void useHistogramme();
 
 
 int mainCore() {
 	bool isOk = true;
 	//isOk &= usePI();
-	isOk &= useMontecarlo();
+	//isOk &= useMontecarlo();
+	useHistogramme();
 
 	cout << "\nisOK = " << isOk << endl;
 	cout << "\nEnd : mainCore" << endl;
