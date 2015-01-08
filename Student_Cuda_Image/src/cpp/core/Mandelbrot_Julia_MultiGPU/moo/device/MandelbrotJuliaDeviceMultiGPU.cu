36135d23c245822780db69d8809a842cc26c5314
piller.yann@gmail.com <piller.yann@gmail.com>
2015-01-08 09:48:58 +0100

    -CHanges names

---
+++ Student_Cuda_Image/src/cpp/core/Mandelbrot_Julia_MultiGPU/moo/device/MandelbrotJuliaDeviceMultiGPU.cu
@@ -0,0 +1,80 @@
+#include "Indice2D.h"
+#include "IndiceTools.h"
+#include "DomaineMath.h"
+#include "cudaTools.h"
+#include "Device.h"
+#include "MandelbrotJuliaMultiGPU.h"
+
+
+
+/*----------------------------------------------------------------------*\
+ |*			Declaration 					*|
+ \*---------------------------------------------------------------------*/
+
+/*--------------------------------------*\
+ |*		Imported	 	*|
+ \*-------------------------------------*/
+
+/*--------------------------------------*\
+ |*		Public			*|
+ \*-------------------------------------*/
+
+__global__ void mandelbrotJuliaCu(uchar4* ptrDevPixels,int w, int h,DomaineMath domaineMath, int n,float t,bool isJulia,float cX,float cY);
+
+/*--------------------------------------*\
+ |*		Private			*|
+ \*-------------------------------------*/
+
+
+
+/*----------------------------------------------------------------------*\
+ |*			Implementation 					*|
+ \*---------------------------------------------------------------------*/
+
+/*--------------------------------------*\
+ |*		Public			*|
+ \*-------------------------------------*/
+
+/*--------------------------------------*\
+ |*		Private			*|
+ \*-------------------------------------*/
+
+__global__ void mandelbrotJuliaCu(uchar4* ptrDevPixels, int w, int h, DomaineMath domaineMath, int n, float t,bool isJulia,float cX,float cY)
+    {
+    MandelbrotJuliaMultiGPU mandelbrotJuliaMath = MandelbrotJuliaMultiGPU(n,isJulia,cX,cY);
+
+    const int TID = Indice2D::tid();
+    const int NB_THREAD = Indice2D::nbThread();
+
+    const int WH=w*h;
+
+    uchar4 color;
+
+    double x;
+    double y;
+
+    int pixelI;
+    int pixelJ;
+
+    int s = TID;
+    while (s < WH)
+	{
+	IndiceTools::toIJ(s, w, &pixelI, &pixelJ); // update (pixelI, pixelJ)
+
+	// (i,j) domaine ecran
+	// (x,y) domaine math
+	domaineMath.toXY(pixelI, pixelJ, &x, &y); //  (i,j) -> (x,y)
+	
+	mandelbrotJuliaMath.colorXY(&color,x, y,domaineMath,t); // update color
+
+	ptrDevPixels[s] = color;
+
+	s += NB_THREAD;
+	}
+
+    }
+
+/*----------------------------------------------------------------------*\
+ |*			End	 					*|
+ \*---------------------------------------------------------------------*/
+
