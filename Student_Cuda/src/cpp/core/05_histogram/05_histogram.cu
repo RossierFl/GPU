964368ac5f60f1be64860688eecc5d4a6844b941
mse15 <mse15@cuda1.(none)>
2015-01-12 20:31:03 +0100

    voila marco, bisou

--- Student_Cuda/src/cpp/core/05_histogram/05_histogram.cu
+++ Student_Cuda/src/cpp/core/05_histogram/05_histogram.cu
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <math.h>
 #include <limits.h>
+#include "AleaTools.h"
 
 //#include <curand_kernel.h>
 #include "Lock.h"
@@ -36,11 +37,12 @@ __host__  bool isHistogram_Ok(void);
  \*-------------------------------------*/
 
 __host__  static bool computeHistogram(int n);
-__global__ static void computeHistogramGPU(int n,bool* ptrDevRes, int ptrDevTabData[], float* ptrDevTabHisto,int lenght);
+__global__ static void computeHistogramGPU(int n,bool* ptrDevRes, int* ptrDevTabData, int* ptrDevTabHisto,int lenght);
 __global__ static void initTabData(curandState* tabGeneratorThread,int n,int ptrDevTabData[], int lenght);
-__device__ void reduce_intra_thread_histogram(int ptrDevTabData[], float* tabSM, int n);
-__device__ bool checkHistogram(float* ptrDevRes,int lenght,int n);
-__device__ static void reduce_inter_block_histogram(float* tabSM, float* tabHistogramGM);
+__device__ void reduce_intra_thread_histogram(int* ptrDevTabData, int* tabSM, int n);
+__device__ bool checkHistogram(int* ptrDevRes,int lenght,int n);
+__device__ static void reduce_inter_block_histogram(int* tabSM, int* tabHistogramGM);
+__host__ void fillAndSwapData(int* ptrHostData, int size);
 #define HISTOGRAM_SIZE 256
 
 
@@ -78,8 +80,10 @@ __host__ bool computeHistogram(int n)
     // Specifier nb thread : ici 1 thread au total !
     //dim3 dg = dim3(2,1,1);
     //dim3 db = dim3(4,1,1);
-    dim3 dg = dim3(2,1,1);
-    dim3 db = dim3(4,1,1);
+    //dim3 dg = dim3(16,1,1);
+    //dim3 db = dim3(32,1,1);
+    dim3 dg = dim3(16,1,1);
+    dim3 db = dim3(32,1,1);
 
     // Debug
     Device::checkDimError(dg,db);
@@ -90,36 +94,38 @@ __host__ bool computeHistogram(int n)
     HANDLE_ERROR(cudaMemset(ptrDevRes,0,size_res)) ;
     //HANDLE_ERROR(cudaMemcpy(ptrRes, ptrDevRes, size_res, cudaMemcpyDeviceToHost));// Device -> Host
 
-    //init tab data
+    int* ptrHostData = new int[n];
+    fillAndSwapData(ptrHostData,n);
+    //copy tab data
     int* ptrDevTabData;
     size_t sizeTabData = sizeof(int)*n;
     HANDLE_ERROR(cudaMalloc((void**) &ptrDevTabData, sizeTabData)); // Device memory allocation (*)
-    HANDLE_ERROR(cudaMemset(ptrDevTabData,0,sizeTabData)) ;
+    HANDLE_ERROR(cudaMemcpy(ptrDevTabData,ptrHostData,sizeTabData,cudaMemcpyHostToDevice)) ;
 
     //init tab histogram final
-    float* ptrDevTabHisto;
-    size_t sizeTabHisto = sizeof(float)*HISTOGRAM_SIZE;
+    int* ptrDevTabHisto;
+    size_t sizeTabHisto = sizeof(int)*HISTOGRAM_SIZE;
     HANDLE_ERROR(cudaMalloc((void**) &ptrDevTabHisto, sizeTabHisto)); // Device memory allocation (*)
     HANDLE_ERROR(cudaMemset(ptrDevTabHisto,0,sizeTabHisto));
 
-    //init random generator
-    curandState* ptrDevRandom;
+    /*//init random generator
+    curandState* ptrDevRandom; //will be done in a better version
     size_t sizeRandom = sizeof(curandState);
     HANDLE_ERROR(cudaMalloc((void**) &ptrDevRandom, sizeRandom)); // Device memory allocation (*)
     HANDLE_ERROR(cudaMemset(ptrDevRandom,0,sizeRandom));
     setup_kernel_rand<<<dg,db>>>(ptrDevRandom,Device::getDeviceId());
-    Device::checkKernelError("setup_kernel_rand");
+    Device::checkKernelError("setup_kernel_rand");*///will be done in a better version
 
     //init tabData
-    initTabData<<<dg,db>>>(ptrDevRandom,n,ptrDevTabData,HISTOGRAM_SIZE);
-    Device::checkKernelError("initTabData"); // facultatif
+    /*initTabData<<<dg,db>>>(ptrDevRandom,n,ptrDevTabData,HISTOGRAM_SIZE);
+    Device::checkKernelError("initTabData"); // facultatif*/
 
 
     int length = HISTOGRAM_SIZE;
-    size_t sizeTabSM = sizeof(float)*length;
+    size_t sizeTabSM = sizeof(int)*length;
     assert((length&(length-1))==0&&"Whoops, length must be a power of two !");
     //int n,bool* ptrRes,float* ptrDevRes, float* ptrDevTabData,int lenght
-    //computeHistogramGPU<<<dg,db,sizeTabSM>>>(n,ptrDevRes, ptrDevTabData,ptrDevTabHisto,HISTOGRAM_SIZE); // asynchrone !!
+    computeHistogramGPU<<<dg,db,sizeTabSM>>>(n,ptrDevRes, ptrDevTabData,ptrDevTabHisto,HISTOGRAM_SIZE); // asynchrone !!
     Device::checkKernelError("computeHistogramGPU"); // facultatif
 
     //v1
@@ -127,16 +133,45 @@ __host__ bool computeHistogram(int n)
 
     // memoryManagement => barrier de synchronisation
     HANDLE_ERROR(cudaMemcpy(ptrRes, ptrDevRes, size_res, cudaMemcpyDeviceToHost));// Device -> Host
+    if(res){
+	printf("Result of histogramm computation : SUCCESS");
+    }else{
+	printf("Result of histogramm computation : FAIL");
+    }
+
     HANDLE_ERROR(cudaFree(ptrDevRes)); // device dispose memory in (*)
     HANDLE_ERROR(cudaFree(ptrDevTabData)); // device dispose memory in (*)
     HANDLE_ERROR(cudaFree(ptrDevTabHisto)); // device dispose memory in (*)
-    HANDLE_ERROR(cudaFree(ptrDevRandom)); // device dispose memory in (*)
+    //HANDLE_ERROR(cudaFree(ptrDevRandom)); // device dispose memory in (*)
     //HANDLE_ERROR(cudaFree(ptrDevTabData)); // device dispose memory in (*)
     return res;
     }
 
+__host__ void fillAndSwapData(int* ptrHostData, int size){
 
-__global__ static void initTabData(curandState* ptrDevTabGeneratorThread,int n,int ptrDevTabData[], int lenght){
+    //fill the data
+    for(int i=0;i<size;i++)
+      {
+	ptrHostData[i]=i%HISTOGRAM_SIZE;
+      }
+
+    //swap
+    AleaTools at;
+    for(int i=0;i<size*size;i++)
+      {
+	  int a = at.uniformeAB(0,size-1);
+	  int b = at.uniformeAB(0,size-1);
+	  int tmpA = ptrHostData[a];
+	  int tmpB = ptrHostData[b];
+	  ptrHostData[a]=tmpB;
+	  ptrHostData[b] = tmpA;
+      }
+}
+
+
+
+
+__global__ static void initTabData(curandState* ptrDevTabGeneratorThread,int n,int ptrDevTabData[], int lenght){ // try to do it on gpu, will be done in a better version
 	  //init curand
 	  /*int tid = Indice1D::tid();
 	  // Global Memory -> Register
@@ -185,62 +220,64 @@ __global__ static void initTabData(curandState* ptrDevTabGeneratorThread,int n,i
 
 }
 
-__global__ void computeHistogramGPU(int n,bool* ptrDevRes, int ptrDevTabData[], float* ptrDevTabHisto,int lenght)
+__global__ void computeHistogramGPU(int n,bool* ptrDevRes, int* ptrDevTabData, int* ptrDevTabHisto,int lenght)
     {
 
     //une instance par block
       //init tab
       const int TID_LOCAL = Indice1D::tidLocalBlock();
-      extern __shared__ float tabSM[];
+      extern __shared__ int tabSM[];
 
 
       UtilsCUDA::initialise(tabSM,lenght);
       UtilsCUDA::initialise(ptrDevTabHisto,lenght);
       reduce_intra_thread_histogram(ptrDevTabData,tabSM, n);
+      __syncthreads();
       reduce_inter_block_histogram(tabSM,ptrDevTabHisto);
       *ptrDevRes = checkHistogram(ptrDevTabHisto,lenght,n);
   }
 
-__device__ void reduce_inter_block_histogram(float* tabSM, float* tabHistogramGM){
+__device__ void reduce_inter_block_histogram(int* tabSM, int* tabHistogramGM){
 	  const int NBTHREAD = Indice1D::nbThread();
+	  const int NBTHREADBLOCK = Indice1D::nbThreadBlock();
           const int TID = Indice1D::tid();
-          const int TID_LOCAL = Indice1D::tidLocalBlock();
+          const int TID_LOCAL = Indice1D::tidLocal();
 
-          int s = TID;
+          int s = TID_LOCAL;
           while(s<HISTOGRAM_SIZE){
               atomicAdd(&tabHistogramGM[s],tabSM[s]);
-              //atomicAdd(&tabSM[0],1);
-              s+=NBTHREAD;
+              s+=NBTHREADBLOCK;
           }
 
      }
 
-__device__ bool checkHistogram(float* ptrDevRes,int lenght,int n){
+__device__ bool checkHistogram(int* ptrDevRes,int lenght,int n){
 	const int NBTHREAD = Indice1D::nbThread();
         const int TID = Indice1D::tid();
         const int TID_LOCAL = Indice1D::tidLocalBlock();
 
         int s = TID;
         bool isOK = true;
-        while(isOK&&s<n){
-            //sumCurrentThread += v(s)*w(s);
-  	  if(isOK){
-  	      if(ptrDevRes[s]!=n/HISTOGRAM_SIZE)isOK=false;
-  	  }
+        int expectedResult = n/HISTOGRAM_SIZE;
+        while(isOK&&s<lenght){
+	    if(ptrDevRes[s]!=expectedResult)isOK=false;
             s+=NBTHREAD;
         }
+        __syncthreads();
         return isOK;
 }
 
 
-__device__ void reduce_intra_thread_histogram(int ptrDevTabData[], float* tabSM, int n){
+__device__ void reduce_intra_thread_histogram(int* ptrDevTabData, int* tabSM, int n){
       const int NBTHREAD = Indice1D::nbThread();
       const int TID = Indice1D::tid();
       const int TID_LOCAL = Indice1D::tidLocalBlock();
 
       int s = TID;
       while(s<n){
-	  atomicAdd(&tabSM[(int)ptrDevTabData[s]],1);
+	  int cValue= ptrDevTabData[s];
+
+	  atomicAdd(&tabSM[cValue],1);
           s+=NBTHREAD;
       }
 }
