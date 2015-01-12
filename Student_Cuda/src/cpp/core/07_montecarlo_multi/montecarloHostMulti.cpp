b8449a3570e2af4f02b36778e7ee052375565d78
mse14 <mse14@cuda1.(none)>
2015-01-12 15:32:05 +0100

    -Change variable to constantes

--- Student_Cuda/src/cpp/core/07_montecarlo_multi/montecarloHostMulti.cpp
+++ Student_Cuda/src/cpp/core/07_montecarlo_multi/montecarloHostMulti.cpp
@@ -55,40 +55,41 @@ bool MontecarloHostMulti::myCalculateMontecarloMulti(void)
     float xMin = 0.0;
     float xMax = 2.0;
     int m = 1;
-
+    const int NBR_GPU=6;
+    const int NBR_THREAD_PER_BLOCK = 4;
     //isOk&= isMontecarlo_Ok(n, xMin, xMax, m);
 
 
     //printf("Montecarlo[n=%l, min=%f, max=%f, m=%d]\n", n, xMin, xMax, m);
     std::cout<<"Montecarlo start datat n : "<<n<<" min : "<< xMin << " max : "<< xMax << " m : "<< m << std::endl;
 
-    float* resultHost = new float[6];
-    	assert(n%6==0);//could be more generic
-    	int nbNPerGPU = n/6;
-    	int nThreadPerBlock = 4;
+    float* resultHost = new float[NBR_GPU];
+    	assert(n%NBR_GPU==0);//could be more generic
+    	int nbNPerGPU = n/NBR_GPU;
+
     	dim3 dg(1, 1, 1); // TODO to optimize
-    	dim3 db(nThreadPerBlock, 1, 1); // TODO to optimize
+    	dim3 db(NBR_THREAD_PER_BLOCK, 1, 1); // TODO to optimize
     	Device::checkDimError(dg, db);
     	Device::checkDimOptimiser(dg, db);
 
     	Device::checkKernelError("setup_kernel_rand_montecarloMulti");
-    	int nbGPU = 6; //could be more generic
-    	omp_set_num_threads(nbGPU);
+
+    	omp_set_num_threads(NBR_GPU);
     #pragma omp parralel for
-    	for(int i=0;i<nbGPU;i++)
+    	for(int i=0;i<NBR_GPU;i++)
     	  {
     	    cudaSetDevice(i);
     	   // printf("i:%d\n",i);
 
     	    //init random generator
     	    curandState* ptrDevRandom;
-    	    size_t sizeRandom = sizeof(curandState) * nThreadPerBlock;
+    	    size_t sizeRandom = sizeof(curandState) * NBR_THREAD_PER_BLOCK;
     	    HANDLE_ERROR(cudaMalloc((void**) &ptrDevRandom, sizeRandom));
     	    HANDLE_ERROR(cudaMemset(ptrDevRandom, 0, sizeRandom));
 
     	    float* ptrDevGM = NULL;
     	    size_t size = sizeof(float);
-    	    size_t sizeTabSM = sizeof(float) * nThreadPerBlock;
+    	    size_t sizeTabSM = sizeof(float) * NBR_THREAD_PER_BLOCK;
     	    HANDLE_ERROR(cudaMalloc(&ptrDevGM, size));
     	    HANDLE_ERROR(cudaMemset(ptrDevGM, 0, size));
 
@@ -101,11 +102,11 @@ bool MontecarloHostMulti::myCalculateMontecarloMulti(void)
     	    HANDLE_ERROR(cudaMemcpy(&resultHost[i], ptrDevGM, size, cudaMemcpyDeviceToHost)); // barrière de synchronisation
     	  }
     	float result = 0.0;
-    	for(int i=0;i<nbGPU;i++)//could be more generic
+    	for(int i=0;i<NBR_GPU;i++)//could be more generic
     	  {
     	      result += resultHost[i];
     	  }
-    	result /= nbGPU;
+    	result /= NBR_GPU;
 
 
     	// Affichage du résultat
